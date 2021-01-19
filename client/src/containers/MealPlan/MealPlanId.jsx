5826294dc59f3bed8490f48fe0903bd0c19c048b
mooretrev <47619174+mooretrev@users.noreply.github.com>
2021-01-19 12:44:23 -0600

    added clickable to meal plan recipes (#14)

--- client/src/containers/MealPlan/MealPlanId.jsx
+++ client/src/containers/MealPlan/MealPlanId.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRouteMatch, useHistory } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -10,10 +11,12 @@ import DeleteMealPlan from '../../api/MealPlan/DeleteMealPlan.js';
 import getUniqueItems from '../../api/MealPlan/getUniqueItems.js';
 import ShoppingList from '../../components/MealPlan/ShoppingList.jsx';
 import GetIngredients from '../../api/MealPlan/GetIngredients.js';
+import GetRecipes from '../../api/Recipes/GetRecipes.js';
 
 export default function MealPlanId() {
   const [mealPlan, setMealPlan] = useState({});
   const [ingredients, setIngredients] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
   const match = useRouteMatch();
   const history = useHistory();
@@ -26,6 +29,8 @@ export default function MealPlanId() {
       setMealPlan(mealPlanCopy);
       const ingredientsCopy = await GetIngredients(token, getUniqueItems(mealPlanCopy));
       setIngredients(ingredientsCopy);
+      const recipesCopy = await GetRecipes(token);
+      setRecipes(recipesCopy);
     };
 
     fetchData();
@@ -33,6 +38,17 @@ export default function MealPlanId() {
 
   const handleBackClick = () => {
     history.push('/mealplan');
+  };
+
+  const handleCardClick = (event, recipeName) => {
+    for (let i = 0; i < recipes.length; i += 1) {
+      if (recipes[i].recipe_name === recipeName) {
+        const id = recipes[i]._id;
+        history.push(`/recipes/${id}`);
+        return 0;
+      }
+    }
+    return 0;
   };
 
   const handleEdit = () => {
@@ -47,13 +63,15 @@ export default function MealPlanId() {
     history.push('/mealplan');
   };
 
-  const renderRecipes = (recipes) => {
+  const renderRecipes = (recipeNames) => {
     const items = [];
-    for (let i = 0; i < recipes.length; i += 1) {
+    for (let i = 0; i < recipeNames.length; i += 1) {
       items.push(
-        <Typography key={`recipes${i}`} variant="h6">
-          {recipes[i]}
-        </Typography>,
+        <CardActionArea key={`recipes${i}`} onClick={(event) => handleCardClick(event, recipeNames[i])}>
+          <Typography variant="h6">
+            {recipeNames[i]}
+          </Typography>
+        </CardActionArea>,
       );
     }
     return items;
