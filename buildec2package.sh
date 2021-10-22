3ac142f3e583e3afd1f54744d4038d36dc07f6aa
mooretrev <47619174+mooretrev@users.noreply.github.com>
2021-10-22 15:04:35 -0500

    Docker compose changes (#46)

--- buildec2package.sh
+++ buildec2package.sh
@@ -2,5 +2,6 @@ rm -rf ec2package
 mkdir ec2package
 cp docker-compose.yml ./ec2package
 cp docker.env ./ec2package
+cp .env ./ec2package
 cp -r reverse_proxy ./ec2package
 zip -r ec2package.zip ec2package
